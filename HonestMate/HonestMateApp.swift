a224093a5ce88a58e9f7125aa5bf895db3a813a3
Karina Gurachevskaya <k.gurachevskaya@pixelplex.io>
2022-08-26 16:55:22 +0200

    Project init

---
+++ HonestMate/HonestMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HonestMateApp.swift
+//  HonestMate
+//
+//  Created by Karina gurachevskaya on 26.08.22.
+//
+
+import SwiftUI
+
+@main
+struct HonestMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,17 @@
+platform :ios, '16.0'
+use_frameworks!
+
+target 'HonestMate' do
+ 
+  pod 'R.swift'
+
+  target 'HonestMateTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'HonestMateUITests' do
+    # Pods for testing
+  end
+
+end
